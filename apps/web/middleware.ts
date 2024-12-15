b0e4d0d5cae45e243be35927d1ac74f30de0081c
sparrow-js <sparrowwht7@gmail.com>
2024-12-15 23:09:32 +0800

    init

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,8 +1,68 @@
 import { auth } from "@/lib/auth";
-import { NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 // Or like this if you need to do something here.
-export default auth((req) => {
+// export default auth((req) => {
+//   // 获取当前路径
+//   const { pathname } = req.nextUrl;
+
+//   const url = req.nextUrl;
+
+//   console.log("*******req.url11", req.headers.get("host"));
+
+//   const hostname = req.headers.get("host").replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+//   const searchParams = req.nextUrl.searchParams.toString();
+//   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+//   const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+
+//   console.log("*******hostname", hostname, path, req.headers.get("host"));
+
+//   // 定义无需认证的路径列表，包括根路径
+//   const publicPaths = ["/", "/login", "/register", "/forgot-password"];
+
+//   if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+//     console.log("*******req.url", new URL(`/app${path === "/" ? "" : path}`, req.url), req.url);
+
+//     if (!req.auth && path !== "/login") {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
+
+//     if (req.auth && path === "/login") {
+//       return NextResponse.rewrite(new URL("/", req.url));
+//     }
+
+//     if (!req.auth && path === "/login") {
+//       return NextResponse.rewrite(new URL("/login", req.url));
+//     }
+
+//     return NextResponse.rewrite(new URL(`/app${path === "/" ? "" : path}`, req.url));
+//   }
+
+//   if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
+//     return NextResponse.rewrite(new URL(`/home${path === "/" ? "" : path}`, req.url));
+//   }
+
+//   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+
+//   // // 如果已登录且访问登录页，重定向到应用首页
+//   // if (req.auth && pathname === "/login") {
+//   //   return Response.redirect(new URL("/app", req.nextUrl.origin));
+//   // }
+
+//   // // 如果用户未认证且当前路径不在公开路径列表中，重定向到登录页
+//   // if (!req.auth && !publicPaths.includes(pathname)) {
+//   //   const newUrl = new URL("/login", req.nextUrl.origin);
+//   //   newUrl.searchParams.set("callbackUrl", pathname);
+//   //   return Response.redirect(newUrl);
+//   // }
+// });
+
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.svg|FascinateInline-Regular\\.ttf).*)"],
+};
+
+export default async function middleware(req: NextRequest) {
   // 获取当前路径
   const { pathname } = req.nextUrl;
 
@@ -22,16 +82,17 @@ export default auth((req) => {
 
   if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
     console.log("*******req.url", new URL(`/app${path === "/" ? "" : path}`, req.url), req.url);
+    const session = await auth();
 
-    if (!req.auth && path !== "/login") {
+    if (!session && path !== "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    if (req.auth && path === "/login") {
+    if (session && path === "/login") {
       return NextResponse.rewrite(new URL("/", req.url));
     }
 
-    if (!req.auth && path === "/login") {
+    if (!session && path === "/login") {
       return NextResponse.rewrite(new URL("/login", req.url));
     }
 
@@ -55,11 +116,4 @@ export default auth((req) => {
   //   newUrl.searchParams.set("callbackUrl", pathname);
   //   return Response.redirect(newUrl);
   // }
-});
-
-// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.png|.*\\.svg|FascinateInline-Regular\\.ttf).*)"],
-};
-
-// export default middleware;
+}
