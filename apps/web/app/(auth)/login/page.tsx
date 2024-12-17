6e752657ebc1f9f27d67c76251ba66c3c2284f34
sparrow-js <sparrowwht7@gmail.com>
2024-12-17 09:41:46 +0800

    init

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import { Suspense } from "react";
 import LoginGithubButton from "./login-git-button";
 import LoginGoogleButton from "./login-google-button";
-// import LoginButton from "./login-button";
-import LoginNotionButton from "./login-notion-button";
 export default function LoginPage() {
   return (
     <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md p-4">
@@ -38,7 +36,7 @@ export default function LoginPage() {
         </Suspense>
       </div>
 
-      <div className="mx-auto mt-2 w-full">
+      {/* <div className="mx-auto mt-2 w-full">
         <Suspense
           fallback={
             <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
@@ -46,7 +44,7 @@ export default function LoginPage() {
         >
           <LoginNotionButton />
         </Suspense>
-      </div>
+      </div> */}
     </div>
   );
 }
