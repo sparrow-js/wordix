2e46022c877cd852ea8c8965a70c747adb56bce8
sparrow-js <sparrowwht7@gmail.com>
2024-12-18 15:02:51 +0800

    init

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -58,16 +58,4 @@ export default async function middleware(req: NextRequest) {
   }
 
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
-
-  // // 如果已登录且访问登录页，重定向到应用首页
-  // if (req.auth && pathname === "/login") {
-  //   return Response.redirect(new URL("/app", req.nextUrl.origin));
-  // }
-
-  // // 如果用户未认证且当前路径不在公开路径列表中，重定向到登录页
-  // if (!req.auth && !publicPaths.includes(pathname)) {
-  //   const newUrl = new URL("/login", req.nextUrl.origin);
-  //   newUrl.searchParams.set("callbackUrl", pathname);
-  //   return Response.redirect(newUrl);
-  // }
 }
