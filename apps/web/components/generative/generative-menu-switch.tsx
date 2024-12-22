997a2c0c7abe9e7a7ed916d9ec2c29052b5e92c6
sparrow-js <sparrowwht7@gmail.com>
2024-12-22 15:46:23 +0800

    init

--- apps/web/components/generative/generative-menu-switch.tsx
+++ apps/web/components/generative/generative-menu-switch.tsx
@@ -1,6 +1,7 @@
 import { EditorBubble, useEditor } from "@/components/headless/components";
 import { removeAIHighlight } from "@/components/headless/extensions";
 import { Fragment, type ReactNode, useEffect } from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import Magic from "../ui/icons/magic";
 import { AISelector } from "./ai-selector";
@@ -12,6 +13,7 @@ interface GenerativeMenuSwitchProps {
 }
 const GenerativeMenuSwitch = ({ children, open, onOpenChange }: GenerativeMenuSwitchProps) => {
   const { editor } = useEditor();
+  const [nodeType, setNodeType] = useState<string | null>(null);
 
   useEffect(() => {
     if (!open) removeAIHighlight(editor);
@@ -24,21 +26,30 @@ const GenerativeMenuSwitch = ({ children, open, onOpenChange }: GenerativeMenuSw
           onOpenChange(false);
           editor.chain().unsetHighlight().run();
         },
+        onShow: () => {
+          console.log("onShow");
+          const selection = editor.view.state.selection;
+          const { $from } = selection;
+          const currentNode = $from.node($from.depth);
+          setNodeType(currentNode.type.name);
+        },
       }}
       className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
     >
       {open && <AISelector open={open} onOpenChange={onOpenChange} />}
       {!open && (
         <Fragment>
-          <Button
-            className="gap-1 rounded-none text-purple-500"
-            variant="ghost"
-            onClick={() => onOpenChange(true)}
-            size="sm"
-          >
-            <Magic className="h-5 w-5" />
-            Ask AI
-          </Button>
+          {nodeType !== "title" && (
+            <Button
+              className="gap-1 rounded-none text-purple-500"
+              variant="ghost"
+              onClick={() => onOpenChange(true)}
+              size="sm"
+            >
+              <Magic className="h-5 w-5" />
+              Ask AI
+            </Button>
+          )}
           {children}
         </Fragment>
       )}
