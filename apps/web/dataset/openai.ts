9e6ef64efbb559b7b4b007378476acbe6511cf42
wordix <sparrowwht7@gmail.com>
2025-02-10 19:01:34 +0800

    fix

--- apps/web/dataset/openai.ts
+++ apps/web/dataset/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
 export const openai = new OpenAI({
-  apiKey: "sk-Yuxzg619EfPAnyd6rJ6el5xfQQzwFxI1HxS9vBfMC4bnqd2p",
+  apiKey: "",
   baseURL: "https://api.openai.com/v1",
 });
