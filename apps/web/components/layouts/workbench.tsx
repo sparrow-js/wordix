6576602aa968159d5d711a099b5c045baa25448d
wordix <sparrowwht7@gmail.com>
2025-02-12 22:15:58 +0800

    fix layout

--- apps/web/components/layouts/workbench.tsx
+++ apps/web/components/layouts/workbench.tsx
@@ -63,7 +63,7 @@ export const Workbench = () => {
     <div className="flex h-full w-full flex-col relative">
       <div className="w-full h-full">
         <PageBar />
-        <div className="w-full h-[calc(100svh-120px)]">
+        <div className="w-full h-[calc(100svh-120px)] p-2">
           {response && response.document && <TailwindAdvancedEditor response={response} />}
         </div>
       </div>
